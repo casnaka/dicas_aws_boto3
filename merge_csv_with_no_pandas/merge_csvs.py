a9cd54fff54e28882d8c1dc344d259e18e10a0cb
Cassiano Nakaoka <cassianobalatore@gmail.com>
2022-03-13 09:52:36 -0300

    [ADD] Comments

--- merge_csv_with_no_pandas/merge_csvs.py
+++ merge_csv_with_no_pandas/merge_csvs.py
@@ -1,11 +1,14 @@
 import glob
 
-#Pegar caminho completo com todos os csvs e colocar todos dentro de uma lista
+
 path = '/home/user/...'
 file = path + 'data.csv'
+#Pegar caminho completo com todos os csvs e colocar todos dentro de uma lista
 all_files = [i for i in glob.glob(path+ '*csv')]
+#Criando o arquivo que vai receber todos os os dados 
 fout = open(file, 'a')
 for i in range(len(all_files)):
+    #condicao criada caso o arquivo tenha header e precisa ser ignorado apos o segundo arquivo
     if i == 0:
         for l in open(all_files[i]):
             fout.write(l)
